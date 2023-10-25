259abd75f31a0b7854111529ee1baab7fd9b3d5f
Maluh <luisavtrsantos@gmail.com>
2023-10-25 10:45:33 -0300

    DELETE - CRUD Básico.

--- blog/views.py
+++ blog/views.py
@@ -14,7 +14,7 @@ from django.http import HttpResponse
 import json
 from django.core.serializers.json import DjangoJSONEncoder
 
-from django.views.generic.edit import CreateView, UpdateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 
 from django.views.decorators.csrf import csrf_exempt
@@ -158,3 +158,13 @@ class PostUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form):
         messages.success(self.request, self.success_message)
         return super(PostUpdateView, self).form_valid(form)
+
+class PostDeleteView(LoginRequiredMixin, DeleteView):
+    model = Post
+    template_name = 'post/post_confirm_delete_form.html'
+    success_url = reverse_lazy('posts_all')
+    success_message = 'A postagem foi excluída com sucesso.'
+
+def form_valid(self, form):
+    messages.success(self.request, self.success_message)
+    return super(PostDeleteView, self).form_valid(form)
