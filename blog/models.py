c7677f8ba80a352de724c63d677362844d365ca6
Maluh <luisavtrsantos@gmail.com>
2023-11-01 10:19:09 -0300

    Post com autor

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,8 @@
 from django.db import models
 
+from django.contrib.auth import get_user_model
+user = get_user_model()
+
 # Create your models here.
 class Post(models.Model):
     body_text = models.TextField('Texto Principal')
@@ -14,4 +17,11 @@ class Post(models.Model):
         ],
         default=None,
         null=True
+    )
+
+    autor = models.ForeignKey(
+        user,
+        editable = False,
+        on_delete = models.DO_NOTHING,
+        null = True
     )
